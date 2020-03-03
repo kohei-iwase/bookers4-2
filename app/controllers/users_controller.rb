6c54ead3ff6533f0a1a399f5b1a1dc14cbbd92f3
kohei-iwase <me.tekelily@gmail.com>
2020-03-03 02:26:06 +0000

    yeild追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action :baria_user, only: [:update,:edit, :show, :index]
+	#before_action :baria_user
 
   def show
   	@user = User.find(params[:id])
@@ -30,10 +30,10 @@ class UsersController < ApplicationController
   	params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
-  #url直接防止　メソッドを自己定義してbefore_actionで発動。
+  url直接防止　メソッドを自己定義してbefore_actionで発動。
    def baria_user
-  	unless params[:id].to_i == current_user.id
-  		redirect_to user_path(current_user)
+	   unless params[:id].to_i == current_user.id
+  		redirect_to user_path(current_user.id)
   	end
    end
 
