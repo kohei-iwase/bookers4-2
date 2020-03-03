8985d78844f598e3d42e095733b7bc2f3e66d0a5
kohei-iwase <me.tekelily@gmail.com>
2020-03-03 10:05:37 +0000

    Controller Model 作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :books do
+    resource	:book_comments, only: [:create]
+  end
   resources :users, only: [:show, :edit,  :update, :index, :create]
+
   root 'home#top'
   get 'home/about' => "home#about"
 end
