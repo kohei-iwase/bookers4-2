2277419d6684f3f2d0c1ef06fc95c3d98dd8b420
kohei-iwase <me.tekelily@gmail.com>
2020-03-03 13:52:40 +0000

    fav実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books do
     resource	:book_comments, only: [:create, :destroy]
+    resource	:favorites, 	only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit,  :update, :index, :create]
 
