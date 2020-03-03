a5db78a7b5659a6b7706008c40ff2b7453b488e8
kohei-iwase <me.tekelily@gmail.com>
2020-03-03 11:43:52 +0000

    comment実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,11 +4,11 @@ class BookCommentsController < ApplicationController
 		comment = current_user.book_comments.new(book_comment_params)
 		comment.book_id = book.id
 		comment.save
-		redirect_to book_path(book
+		redirect_to book_path(book)
 	end
 
 	private
-	
+
 	def book_comment_params
 		params.require(:book_comment).permit(:comment)
 	end
