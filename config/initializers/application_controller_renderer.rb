52d83e86b5a7281baa749de4c8173ec00424e2a6
kohei-iwase <me.tekelily@gmail.com>
2020-03-04 06:13:31 +0000

    seed導入

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fafe0ca38347a63fb5c6e6137d6226a4e353eab630d01d9928c0cdbf3efb84ed4bb043069715aeca439a828aee9fe5179e4e2d62a04ab22c96ecf0d147fda6fd'
