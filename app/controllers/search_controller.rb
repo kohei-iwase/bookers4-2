8f574256bd1b3020d08a7d0d2616cf11ea030b2e
kohei-iwase <me.tekelily@gmail.com>
2020-03-05 02:33:03 +0000

    検索機能強化

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,13 @@
 class SearchController < ApplicationController
-	def index
-		@s = User.ransack(params[:q])
-		@users =  @q.result
+	
+	def search
+	    @q = User.ransack(params[:q])
+    	@users = @q.result(distinct: true)
+    	# @b = Book.ransack(params[:b])
+    	# @books = @b.result(distinct: true)
+	end
+
+	def searched
+		@result = @q.result(distinct: true)#ここで検索
 	end
 end
