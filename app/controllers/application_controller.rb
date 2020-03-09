89dfb13d0eef977768f9cf575b5c099c9cf2fde3
kohei-iwase <me.tekelily@gmail.com>
2020-03-09 03:56:33 +0000

    環境変数定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,6 +27,7 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
       #sign_upの際にnameのデータ操作を許。追加したカラム。
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :postal_code, :prefecture_code, :city, :street])
     end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,8 +48,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-  	params.require(:user).permit(:name, :introduction, :profile_image)
-    params.require(:user).permit(:postcode, :prefecture_name, :address_city, :address_street, :address_building)
+  	params.require(:user).permit(:name, :introduction, :profile_image,:postcode, :prefecture_name, :address_city, :address_street, :address_building)
   end
 
  # url直接防止　メソッドを自己定義してbefore_actionで発動。
