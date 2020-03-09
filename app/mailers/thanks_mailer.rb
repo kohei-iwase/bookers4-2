fcad778fa4943a83aab5e5ccd9532f0321e1ef1b
kohei-iwase <me.tekelily@gmail.com>
2020-03-09 08:33:49 +0000

    config

---
+++ app/mailers/thanks_mailer.rb
@@ -0,0 +1,7 @@
+class ThanksMailer < ApplicationMailer
+
+  def send_mail(user)
+    @user = user
+    mail to: user.email, subject:"新規登録完了のご連絡"
+  end
+end
---
+++ test/mailers/previews/thanksmailer_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/thanksmailer_mailer
+class ThanksmailerMailerPreview < ActionMailer::Preview
+
+end
